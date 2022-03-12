4c12d3229c6e460ea99ab610b1713c14b8bd3cf8
kbario <kylebario1@gmail.com>
2022-03-12 08:46:44 +0800

    category routes done

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -33,8 +33,9 @@ router.post('/', (req, res) => {
   // create a new category
   try {
     const createCategory = await Category.create({
-      
+      category_name: req.body.category_name
     });
+    res.status(200).json(createCategory)
   } catch (error) {
     res.status(500).json(error)
   }
@@ -42,10 +43,26 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  try {
+    const updateCategory = await Category.update(
+      { category_name: req.body.category_name },
+      { where: { id: req.params.id } });
+    res.status(200).json(updateCategory)
+  } catch (error) {
+    res.status(500).json(error)
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  try {
+    const deleteCategory = await Category.destroy({
+      where: { id: req.params.id}
+    });
+    res.status(200).json(deleteCategory)
+  } catch (error) {
+    res.status(500).json(error)
+  }
 });
 
 module.exports = router;
